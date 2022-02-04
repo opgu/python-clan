1d3b01160172953d886f092ca206d41e4cf19911
opgu <99020484+opgu@users.noreply.github.com>
2022-02-04 17:20:51 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,6 @@
+constant_PI =3.1459
+
+radius =float('input the radius of the circle: ')
+area =constant_PI* radius 
+
+print("the area of the circle is", area)
---
+++ extension java ,html.py
@@ -0,0 +1,11 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python38-32/enter the value of the circle.py
+input the filename: abc.java
+the extension of the file is : 'java'
+>>> 
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python38-32/extension java.py
+input the filename: abe.html
+the extension of the file is : 'html'
+>>> 
