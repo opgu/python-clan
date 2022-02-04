9df2a973f887d6ca6161fedb0258670fbcf065c2
opgu <99020484+opgu@users.noreply.github.com>
2022-02-04 17:38:06 +0530

    Add files via upload

---
+++ value of the circle.py
@@ -0,0 +1,11 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python38-32/enter the value of the circle.py
+input the value of the circle: 5
+the area of the circle is  78.64750000000001
+>>> 
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python38-32/enter the value of the circle.py
+input the value of the circle: 6
+the area of the circle is  113.25240000000001
+>>> 
